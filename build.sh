baedd993cbb064ac33ea4cbe7c728a5f2b904b12
Dragutin Cirkovic <dragonmen@gmail.com>
2015-03-20 16:08:20 -0700

    Added some dependicies to build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt-get install libdvbcsa-dev libiconv-hook-dev libiconv-hook1 libhdhomerun-dev libssl-dev
+sudo apt-get install libdvbcsa-dev libiconv-hook-dev libiconv-hook1 libhdhomerun-dev libssl-dev automake build-essential git debhelper libavahi-client-dev libcurl4-gnutls-dev liburiparser-dev libtool
 ./configure --prefix=/usr --enable-bundle --disable-avahi --enable-libav --enable-libffmpeg_static --enable-dvbcsa --enable-v4l
 make -j4
