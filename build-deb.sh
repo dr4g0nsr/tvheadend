469e99bb0e3980b1846415669abf0ccf7236a37d
Dragutin Cirkovic <dragonmen@gmail.com>
2015-03-20 15:43:28 +0100

    autobuild script

--- build-deb.sh
+++ build-deb.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 apt-get install libdvbcsa-dev libiconv-hook-dev libiconv-hook1 libhdhomerun-dev
-./configure --prefix=/usr --enable-bundle --disable-avahi --enable-libav --enable-libffmpeg_static --enable-dvbcsa --enable-v4l
-make -j4
+AUTOBUILD_CONFIGURE_EXTRA=--enable-bundle ./Autobuild.sh -j4
