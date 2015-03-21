8d1b716d0bc56aa136f7333a9ceece73e3e086ef
Dragutin Cirkovic <dragonmen@gmail.com>
2015-03-21 03:15:05 -0700

    yasm dependicies

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get install libdvbcsa-dev libiconv-hook-dev libiconv-hook1 libhdhomerun-dev libssl-dev automake build-essential git debhelper libavahi-client-dev libcurl4-gnutls-dev liburiparser-dev libtool
+sudo apt-get install libdvbcsa-dev libiconv-hook-dev libiconv-hook1 libhdhomerun-dev libssl-dev automake build-essential git debhelper libavahi-client-dev libcurl4-gnutls-dev liburiparser-dev libtool yasm
 ./configure --prefix=/usr --enable-bundle --disable-avahi --enable-libav --enable-libffmpeg_static --enable-dvbcsa --enable-v4l
 make -j4
